a7f52770cba1814161485736c94efe6d27cb4dd2
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-02-23 11:29:08 +0000

    implement and test

--- index.js
+++ index.js
@@ -1,3 +1,116 @@
-module.exports = function (AWS, config) {
+'use strict';
 
+/**
+ * Create the utility that sends elasticsearch request
+ *
+ * @param {aws-sdk} AWS sdk
+ * @param {Object} config Configuration containing
+ *    * endpoint: Elasticsearch endpoint
+ *    * region: AWS region
+ * @return {Object} Utility with method `send`
+ */
+module.exports = function (AWS, config) {
+	var awsEndpoint = new AWS.Endpoint(config.endpoint);
+
+	function sendRequest (params, callback) {
+		var req = new AWS.HttpRequest(awsEndpoint);
+		req.method = params.method.toUpperCase();
+		req.path = params.path;
+		req.region = config.region;
+		req.headers['presigned-expires'] = false;
+		req.headers['Host'] = awsEndpoint.host;
+		if (params.message) {
+			req.body = typeof params.message == 'string' ? params.message : JSON.stringify(params.message);
+		}
+
+		var creds = new AWS.EnvironmentCredentials('AWS');
+
+		var signer = new AWS.Signers.V4(req , 'es');
+		signer.addAuthorization(creds, new Date());
+
+		var send = new AWS.NodeHttpClient();
+		send.handleRequest(req, null, function (httpResp) {
+			handleSuccess(httpResp, params.json === false, callback);
+		}, function (err) {
+			handleFail(err, callback);
+		});
+	}
+
+	function handleSuccess (httpResp, asPlainText, callback) {
+		var respBody = '';
+		httpResp.on('data', function (chunk) {
+			respBody += chunk;
+		});
+		httpResp.on('end', function () {
+			if (httpResp.statusCode >= 200 && httpResp.statusCode < 400) {
+				if (asPlainText) {
+					process.nextTick(function () {
+						callback(null, respBody);
+					});
+				} else {
+					try {
+						var response = JSON.parse(respBody);
+						process.nextTick(function () {
+							callback(null, response);
+						});
+					} catch (ex) {
+						var error = new Error('Invalid JSON response');
+						error.responseText = respBody;
+						process.nextTick(function () {
+							callback(error);
+						});
+					}
+				}
+			} else {
+				var errorMessage;
+				try {
+					var errorResponse = JSON.parse(respBody);
+					errorMessage = errorResponse.message;
+				} catch (ex) {
+					errorMessage = respBody;
+				}
+				process.nextTick(function () {
+					callback(new Error(errorMessage));
+				});
+			}
+		});
+	}
+
+	function handleFail (err, callback) {
+		var error = typeof err === 'string' ? new Error(err) : err;
+		process.nextTick(function () {
+			callback(error);
+		});
+	}
+
+	return {
+		/**
+		 * Send a request to ElastiSearch
+		 * @param {Object} params Object containing
+		 *    * method: HTTP method (e.g GET, POST)
+		 *    * path: Request path (e.g. '/_cat/indices')
+		 *    * message: Request body (String or JSON)
+		 *    * json: Boolean, whether the response from the server should be parsed as JSON
+		 * @return {[type]} [description]
+		 */
+		send: function (params, callback) {
+			if (!params) {
+				process.nextTick(function () {
+					callback(new TypeError('Missing or invalid parameters'));
+				});
+			} else if (!params.method) {
+				process.nextTick(function () {
+					callback(new TypeError('Missing method'));
+				});
+			} else {
+				try {
+					sendRequest(params, callback);
+				} catch (ex) {
+					process.nextTick(function () {
+						callback(ex);
+					});
+				}
+			}
+		}
+	};
 };
--- test.js
+++ test.js
@@ -1,3 +1,346 @@
-var tap = require('tap');
-var es = require('./index');
+'use strict';
 
+var tap = require('tap');
+var elastic = require('./index');
+var EventEmitter = require('events').EventEmitter;
+
+var testConfig = {
+	region: 'eu-west-1',
+	endpoint: 'elastic.search.es.amazonaws.com'
+};
+
+var AWS = function (implementations) {
+	var Endpoint = [],
+		HttpRequest = [],
+		EnvironmentCredentials = [],
+		SignersV4 = [],
+		SignersAddAuthorization = [],
+		NodeHttpClient = [],
+		handleRequest = [];
+
+	return {
+		Endpoint: function () {
+			Endpoint.push([].slice.apply(arguments));
+			return {
+				host: 'aws-host'
+			};
+		},
+		HttpRequest: function () {
+			HttpRequest.push([].slice.apply(arguments));
+			return {
+				headers: {}
+			};
+		},
+		EnvironmentCredentials: function () {
+			EnvironmentCredentials.push([].slice.apply(arguments));
+		},
+		Signers: {
+			V4: function () {
+				SignersV4.push([].slice.apply(arguments));
+				return {
+					addAuthorization: function () {
+						SignersAddAuthorization.push([].slice.apply(arguments));
+					}
+				};
+			}
+		},
+		NodeHttpClient: function () {
+			NodeHttpClient.push([].slice.apply(arguments));
+			return {
+				handleRequest: function () {
+					handleRequest.push([].slice.apply(arguments));
+					implementations.handleRequest.apply(null, arguments);
+				}
+			};
+		},
+		spies: {
+			Endpoint: Endpoint,
+			HttpRequest: HttpRequest,
+			EnvironmentCredentials: EnvironmentCredentials,
+			SignersV4: SignersV4,
+			SignersAddAuthorization: SignersAddAuthorization,
+			NodeHttpClient: NodeHttpClient,
+			handleRequest: handleRequest
+		}
+	};
+};
+
+tap.test('fails on invalid params', function (test) {
+	var aws = AWS({});
+	var es = elastic(aws, testConfig);
+	es.send(null, function (err) {
+		test.type(err, TypeError);
+		test.match(err.message, /missing/i);
+		test.end();
+	});
+});
+
+tap.test('fails on missing method', function (test) {
+	var aws = AWS({});
+	var es = elastic(aws, testConfig);
+	es.send({}, function (err) {
+		test.type(err, TypeError);
+		test.match(err.message, /missing method/i);
+		test.end();
+	});
+});
+
+tap.test('fails on exceptions', function (test) {
+	var aws = AWS({
+		handleRequest: function () {
+			throw new Error('No you can\'t');
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err) {
+		test.type(err, Error);
+		test.match(err.message, /no you/i);
+		test.end();
+	});
+});
+
+tap.test('fails on HTTP client error as string', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData, onError) {
+			onError('invalid string');
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err) {
+		test.type(err, Error);
+		test.match(err.message, /invalid string/i);
+		test.end();
+	});
+});
+
+tap.test('fails on HTTP client error as error', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData, onError) {
+			onError(new Error('invalid error'));
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err) {
+		test.type(err, Error);
+		test.match(err.message, /invalid error/i);
+		test.end();
+	});
+});
+
+tap.test('fails on AWS status error with plain text', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 400;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('data', 'plain text');
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err) {
+		test.type(err, Error);
+		test.match(err.message, /plain text/i);
+		test.end();
+	});
+});
+
+tap.test('fails on AWS status error with JSON', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 400;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('data', JSON.stringify({
+					message: 'json error'
+				}));
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err) {
+		test.type(err, Error);
+		test.match(err.message, /json error/i);
+		test.end();
+	});
+});
+
+tap.test('works correctly with plain text', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 200;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('data', 'response');
+				emitter.emit('data', ' as ');
+				emitter.emit('data', 'text');
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET',
+		json: false
+	}, function (err, data) {
+		test.equal(data, 'response as text');
+		test.end();
+	});
+});
+
+tap.test('fails when JSON cannot be serialized', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 200;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('data', 'response text expecting json');
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err) {
+		test.type(err, Error);
+		test.match(err.message, /invalid json/i);
+		test.equal(err.responseText, 'response text expecting json');
+		test.end();
+	});
+});
+
+tap.test('works correctly with JSON', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 200;
+			onData(emitter);
+
+			process.nextTick(function () {
+				var message = JSON.stringify({
+					results: ['one', 'two']
+				});
+				emitter.emit('data', message.slice(0, 20));
+				process.nextTick(function () {
+					emitter.emit('data', message.slice(20));
+					process.nextTick(function () {
+						emitter.emit('end');
+					});
+				});
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'GET'
+	}, function (err, data) {
+		test.same(data, {
+			results: ['one', 'two']
+		});
+		test.end();
+	});
+});
+
+tap.test('assert request parameters with string message', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 400;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'post',
+		path: 'test'
+	}, function () {
+		test.same(aws.spies.Endpoint, [['elastic.search.es.amazonaws.com']]);
+		test.ok(aws.spies.SignersV4[0][0], 'request should be signed');
+		test.equal(aws.spies.SignersV4[0][0], aws.spies.handleRequest[0][0]);
+		test.same(aws.spies.handleRequest[0][0], {
+			method: 'POST',
+			path: 'test',
+			region: 'eu-west-1',
+			headers: {
+				'presigned-expires': false,
+				Host: 'aws-host'
+			}
+		});
+		test.end();
+	});
+});
+
+tap.test('assert request parameters with string message', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 400;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'HEAD',
+		message: 'some message'
+	}, function () {
+		test.equal(aws.spies.handleRequest[0][0].method, 'HEAD');
+		test.equal(aws.spies.handleRequest[0][0].body, 'some message');
+		test.end();
+	});
+});
+
+tap.test('assert request parameters with JSON message', function (test) {
+	var aws = AWS({
+		handleRequest: function (request, _, onData) {
+			var emitter = new EventEmitter();
+			emitter.statusCode = 400;
+			onData(emitter);
+
+			process.nextTick(function () {
+				emitter.emit('end');
+			});
+		}
+	});
+	var es = elastic(aws, testConfig);
+	es.send({
+		method: 'whatever',
+		message: {
+			type: 'json'
+		}
+	}, function () {
+		test.equal(aws.spies.handleRequest[0][0].method, 'WHATEVER');
+		test.equal(aws.spies.handleRequest[0][0].body, JSON.stringify({
+			type: 'json'
+		}));
+		test.end();
+	});
+});
