0c582e890ccb97f2f9f5ca7f0d8b4f20916b8c18
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-04-07 15:27:56 +0100

    use aws-signed-request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var signer = require('aws-signed-request');
 /**
  * Create the utility that sends elasticsearch request
  *
@@ -10,80 +11,11 @@
  * @return {Object} Utility with method `send`
  */
 module.exports = function (AWS, config) {
-	var awsEndpoint = new AWS.Endpoint(config.endpoint);
-
-	function sendRequest (params, callback) {
-		var req = new AWS.HttpRequest(awsEndpoint);
-		req.method = params.method.toUpperCase();
-		req.path = params.path;
-		req.region = config.region;
-		req.headers['presigned-expires'] = false;
-		req.headers['Host'] = awsEndpoint.host;
-		if (params.message) {
-			req.body = typeof params.message == 'string' ? params.message : JSON.stringify(params.message);
-		}
-
-		var creds = new AWS.EnvironmentCredentials('AWS');
-
-		var signer = new AWS.Signers.V4(req , 'es');
-		signer.addAuthorization(creds, new Date());
-
-		var send = new AWS.NodeHttpClient();
-		send.handleRequest(req, null, function (httpResp) {
-			handleSuccess(httpResp, params.json === false, callback);
-		}, function (err) {
-			handleFail(err, callback);
-		});
-	}
-
-	function handleSuccess (httpResp, asPlainText, callback) {
-		var respBody = '';
-		httpResp.on('data', function (chunk) {
-			respBody += chunk;
-		});
-		httpResp.on('end', function () {
-			if (httpResp.statusCode >= 200 && httpResp.statusCode < 400) {
-				if (asPlainText) {
-					process.nextTick(function () {
-						callback(null, respBody);
-					});
-				} else {
-					try {
-						var response = JSON.parse(respBody);
-						process.nextTick(function () {
-							callback(null, response);
-						});
-					} catch (ex) {
-						var error = new Error('Invalid JSON response');
-						error.responseText = respBody;
-						process.nextTick(function () {
-							callback(error);
-						});
-					}
-				}
-			} else {
-				var errorMessage;
-				try {
-					var errorResponse = JSON.parse(respBody);
-					errorMessage = errorResponse.Message;
-				} catch (ex) {
-					errorMessage = respBody;
-				}
-				process.nextTick(function () {
-					var error = new Error(errorMessage);
-					error.responseText = respBody;
-					callback(error);
-				});
-			}
-		});
-	}
-
-	function handleFail (err, callback) {
-		var error = typeof err === 'string' ? new Error(err) : err;
-		process.nextTick(function () {
-			callback(error);
-		});
-	}
+	var elastic = signer(AWS, {
+		endpoint: config.endpoint,
+		region: config.region,
+		service: 'es'
+	});
 
 	return {
 		/**
@@ -96,23 +28,7 @@ module.exports = function (AWS, config) {
 		 * @return {[type]} [description]
 		 */
 		send: function (params, callback) {
-			if (!params) {
-				process.nextTick(function () {
-					callback(new TypeError('Missing or invalid parameters'));
-				});
-			} else if (!params.method) {
-				process.nextTick(function () {
-					callback(new TypeError('Missing method'));
-				});
-			} else {
-				try {
-					sendRequest(params, callback);
-				} catch (ex) {
-					process.nextTick(function () {
-						callback(ex);
-					});
-				}
-			}
+			elastic.send(params, callback);
 		}
 	};
 };
